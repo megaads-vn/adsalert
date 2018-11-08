7233fe531fcde301327c195d09f4f81ccf6de1b3
Phu Luong <phult.contact@gmail.com>
2018-11-08 16:45:57 +0700

    send detail info

--- scripts/googleads.js
+++ scripts/googleads.js
@@ -2,38 +2,80 @@ var ACCOUNT = "TEST123";
 var SERVICE_URL = "http://adsalert.agoz.me/ads/unapprove";
 
 function main() {
-    var accountSelector = MccApp.accounts();//.withIds(["235-905-9992"]);
+    var accountSelector = MccApp.accounts();//.withIds(["744-728-6416"]);
     accountSelector.executeInParallel("run", "finish");
 }
 
 function run() {
-    var retval = 0;
+    var retval = {
+        disapprovedCount: 0,
+        disapprovedList: []
+    };
+    var accountName = AdWordsApp.currentAccount().getName();
     var campIter = AdWordsApp.campaigns().withCondition('Status = ENABLED').get();
-    var disapprovedList = [];
     while (campIter.hasNext()) {
         var camp = campIter.next();
         adsIter = camp.ads().withCondition('Status = ENABLED').withCondition('CombinedApprovalStatus NOT_IN [APPROVED, APPROVED_LIMITED, UNDER_REVIEW]').get();
-        retval += adsIter.totalNumEntities();
-        while (adsIter.hasNext()) {
-            var ad = adsIter.next();
-            Logger.log(camp.getName() + ' ' + ad.getAdGroup().getName() + ' ' + ad.getHeadline() + ' ' + ad.getDescription1() + ' ' + ad.getDescription2() + ' ' + ad.getDisapprovalReasons());
+        if (adsIter.totalNumEntities() > 0) {
+            retval.disapprovedCount += adsIter.totalNumEntities();
+            retval.disapprovedList.push({
+                "accountName": accountName,
+                "campaignName": camp.getName(),
+                "adsCount": adsIter.totalNumEntities()
+            });
         }
+        // while (adsIter.hasNext()) {
+        //     var ad = adsIter.next();
+        //     Logger.log(camp.getName() + ' ' + ad.getAdGroup().getName() + ' ' + ad.getHeadline() + ' ' + ad.getDescription1() + ' ' + ad.getDescription2() + ' ' + ad.getDisapprovalReasons());
+        // }
     }
-    Logger.log("retval= " + retval);
     return JSON.stringify(retval);
 }
 function finish(results) {
     var unApprovedAds = 0;
+    var disapprovedList = [];
     for (var i = 0; i < results.length; i++) {
-        unApprovedAds += parseInt(results[i].getReturnValue());
+        var returnValue = JSON.parse(results[i].getReturnValue());
+        unApprovedAds += parseInt(returnValue.disapprovedCount);
+        disapprovedList = disapprovedList.concat(returnValue.disapprovedList);
     }
+    var message = "<div>";
+    message += "<table>";
+    message += "<tr>";
+    message += "<th>";
+    message += "Account";
+    message += "</th>";
+    message += "<th>";
+    message += "Campaign";
+    message += "</th>";
+    message += "<th>";
+    message += "Disapproved Ads";
+    message += "</th>";
+    message += "</tr>";
+    for (var i = 0; i < disapprovedList.length; i++) {
+        var element = disapprovedList[i];
+        message += "<tr>";
+        message += "<td>";
+        message += element.accountName;
+        message += "</td>";
+        message += "<td>";
+        message += element.campaignName;
+        message += "</td>";
+        message += "<td>";
+        message += element.adsCount;
+        message += "</td>";
+        message += "</tr>";
+    }
+    message += "</table>";
+    message += "</div>";
     var options = {
         "method": "post",
         "payload": {
+            "account": ACCOUNT,
             "count": unApprovedAds,
-            "account": ACCOUNT
+            "message": message
         }
     };
     var response = UrlFetchApp.fetch(SERVICE_URL, options);
-    Logger.log("unApprovedAds= " + unApprovedAds);
+    Logger.log("UrlFetchApp response: " + response);
 }
--- services/app/Http/Controllers/AdsController.php
+++ services/app/Http/Controllers/AdsController.php
@@ -12,13 +12,14 @@ class AdsController extends BaseController
     {
         $unapproved = $request->input('count');
         $account = $request->input('account');
+        $message = $request->input('message');
         $key = 'adwords:unapproved_ads:' . $account;
         $lastUnapproved = Cache::get($key, -1);
         Cache::forever($key, $unapproved);
         \Log::info("Checking Unapproved ads - Account: " . $account . ", lastUnapproved: " . $lastUnapproved . ", unapproved: " . $unapproved);
         if ($lastUnapproved >= 0 && $lastUnapproved < $unapproved) {
             \Log::info("Notify Unapproved ads");
-            $this->sendEmail(env('MAIL_TO'), $account . ' has REJECTED ADS', '');
+            $this->sendEmail(env('MAIL_TO'), $account . ' has DISAPPROVED ADS', $message);
             $this->callPhone(env('CALL_TO'));
         }
         return $this->success(null);
