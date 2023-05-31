e2e4e65d26000f5c580d9f0d423eaabb3bf58e98
Nam <nambcvt275@gmail.com>
2023-05-31 10:47:47 +0700

    optimize: alert log

--- services/app/Http/Controllers/AdsController.php
+++ services/app/Http/Controllers/AdsController.php
@@ -228,7 +228,7 @@ class AdsController extends BaseController
             foreach ($accounts as $account) {
                 $key = $this->getKey('adwords:campaign_cost:' . $account->accountName . ':' . $account->campaignName . ':' . $account->campaignId, $mailTo);
                 $cacheAccount= Cache::get($key, null);
-                $logMessage = "Checking Limit Cost 30 Days - Account: " . $account->accountName . ", Campaign: " . $account->campaignName . " CampaignId: " . $account->campaignId . ", Cost: " . $account->cost;
+                $logMessage = "Checking Limit Cost 30 Days - Account: " . $account->accountName . (isset($account->accountId) ? ", AccountId: " . $account->accountId : "") . ", Campaign: " . $account->campaignName . " CampaignId: " . $account->campaignId . ", Cost: " . $account->cost;
                 if (!empty($cacheAccount) && is_object($cacheAccount)) {
                     $logMessage .= ", Last Cost: " . $cacheAccount->cost;
                     $account->is_send = isset($cacheAccount->is_send) ? $cacheAccount->is_send : 0;
@@ -293,7 +293,7 @@ class AdsController extends BaseController
                 $keyAllTime = $this->getKey('adwords:campaign_cost_all_time:' . $account->accountName . ':' . $account->campaignName . ':' . $account->campaignId, $mailTo);
                 $cacheAccountAllTime = Cache::get($keyAllTime, null);
                 $cacheAccount= Cache::get($key, null);
-                $logMessage = "Checking Limit Cost All Time - Account: " . $account->accountName . ", Campaign: " . $account->campaignName . " CampaignId: " . $account->campaignId . ", Cost: " . $account->cost;
+                $logMessage = "Checking Limit Cost All Time - Account: " . $account->accountName . (isset($account->accountId) ? ", AccountId: " . $account->accountId : "") . (isset($account->accountId) ? "AccountId: " . $account->accountId : "") . " , Campaign: " . $account->campaignName . " CampaignId: " . $account->campaignId . ", Cost: " . $account->cost;
                 if (!empty($cacheAccount) && is_object($cacheAccount) && !empty($cacheAccountAllTime) && is_object($cacheAccountAllTime)) {
                     $logMessage .= ", Last Cost: " . $cacheAccountAllTime->cost;
                     $account->is_send = isset($cacheAccount->is_send) ? $cacheAccount->is_send : 0;
